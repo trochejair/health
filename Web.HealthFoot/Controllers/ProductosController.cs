05894a6f3162e46d11e370a8bb97f92398c575b8
Unknown <leojfl@outlook.com>
2019-11-24 17:24:22 -0600

    Edit product full

--- Web.HealthFoot/Controllers/ProductosController.cs
+++ Web.HealthFoot/Controllers/ProductosController.cs
@@ -127,11 +127,111 @@ namespace Web.HealthFoot.Controllers
             });
         }
 
-        public ActionResult Edit()
+        public ActionResult Edit(int id)
         {
-            return View();
+            HealthEntities db = new HealthEntities();
+            List<INSUMO> supplies = db.INSUMO.ToList();
+            List<CATEGORIA> category = db.CATEGORIA.ToList();
+            ViewBag.insumos = supplies;
+            ViewBag.categotias = category;
+
+            ViewBag.Category = category;
+            ViewBag.Supplies = new SelectList(supplies, "ID", "NOMBRE"); ;
+
+            PRODUCTO product = db.PRODUCTO.Find(id);
+
+            return View(product);
         }
 
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection formCollection)
+        {
+            var success = false;
+            var message = "";
+            var data = 0;
+            List<List<string>> errors = new List<List<string>>();
+
+            var nombre = formCollection["nombre"];
+            var descripcion = formCollection["descripcion"];
+            var cantidad = formCollection["cantidad"];
+            var categoria = formCollection["Category"];
+            var precio = formCollection["precio"];
+
+            if (nombre.Trim().Length == 0)
+            {
+                List<string> name = new List<string>();
+                name.Add("nombre");
+                name.Add("Nombre necesario");
+                errors.Add(name);
+            }
+            if (descripcion.Trim().Length == 0)
+            {
+                List<string> description = new List<string>();
+                description.Add("descripcion");
+                description.Add("Descripcion necesaria");
+                errors.Add(description);
+            }
+            if (cantidad.Trim().Length == 0)
+            {
+                List<string> quantity = new List<string>();
+                quantity.Add("cantidad");
+                quantity.Add("Cantidad necesaria");
+                errors.Add(quantity);
+            }
+            if (precio.Trim().Length == 0)
+            {
+                List<string> price = new List<string>();
+                price.Add("precio");
+                price.Add("Precio necesario");
+                errors.Add(price);
+            }
+
+            if (errors.Count == 0)
+            {
+                using (HealthEntities db = new HealthEntities())
+                {
+                    using (var transaction = db.Database.BeginTransaction())
+                    {
+                        //                 try
+                        //                   {
+                        PRODUCTO product = db.PRODUCTO.Find(id);
+                        product.NOMBRE = nombre;
+                        product.DESCRIPCION = descripcion;
+                        product.CANTIDAD = Int32.Parse(cantidad);
+                        product.ACTIVO = 1;
+                        product.FK_CATEGORIA = Int32.Parse(categoria);
+                        product.PRECIO = float.Parse(precio);
+                        db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                        data = product.ID;
+                        success = true;
+
+
+                        //                    }
+                        // catch
+                        //{
+                        //   transaction.Rollback();
+
+                        //}
+
+
+                    }
+
+                }
+
+
+
+            }
+
+            return Json(new
+            {
+                success,
+                message,
+                data,
+                errors
+            });
+        }
 
         public ActionResult Details()
         {
@@ -165,7 +265,10 @@ namespace Web.HealthFoot.Controllers
                         if (!isExists)
                             System.IO.Directory.CreateDirectory(pathString);
 
-                        var fileNamedb = "img_" + id + System.DateTime.Now + "_" + file.FileName;
+                        Random rnd = new Random();
+                        int rndx = rnd.Next(0, 1000);
+
+                        var fileNamedb = "img_" + id + rndx + "_" + file.FileName;
                         var path = string.Format("{0}\\{1}", pathString, fileNamedb);
                         file.SaveAs(path);
 
@@ -189,6 +292,33 @@ namespace Web.HealthFoot.Controllers
             }
             return Json(new { Message = fName });
         }
+        [HttpPost]
+        public ActionResult DeleteImages(FormCollection formCollection) {
+            var success = false;
+            var message = "";
 
+            var allKey = formCollection.AllKeys;
+            var count = allKey.Length;
+
+            using (HealthEntities db = new HealthEntities()) { 
+                for (var x = 0; x < count; x++)
+                {
+                    string key = allKey[x];
+                    var imageId = formCollection[key];
+                    IMAGEN_PRODUCTO img= db.IMAGEN_PRODUCTO.Find(Int32.Parse(imageId));
+                    db.Entry(img).State = System.Data.Entity.EntityState.Deleted;
+                    db.SaveChanges();
+                }
+                success = true;
+                message = "Se eliminarion imagenes";
+            }
+
+
+            return Json(new
+            {
+                success,
+                message,
+            });
+        }
     }
 }
